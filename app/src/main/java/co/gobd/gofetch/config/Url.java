58d4480d2b31a055b17eb84a04281b17fc1545bf
Fahim Masud Choudhury <fahim@gobd.co>
2016-03-20 17:30:24 +0600

    GFETCH-53 Starting and destination point EditText now have no soft keyboard when pressed. Also, keyboard is not popping now when RideActivity starts

--- app/src/main/java/co/gobd/gofetch/config/Url.java
+++ app/src/main/java/co/gobd/gofetch/config/Url.java
@@ -5,7 +5,7 @@ package co.gobd.gofetch.config;
  */
 public class Url {
     // Supported order
-    public static final String BASE_URL = "http://192.168.1.10:8080";
+    public static final String BASE_URL = "http://192.168.0.136:8080";
     public static final String PATH_SUPPORTED_ORDER = "/api/Order/SupportedOrder.json";
     public static final String BASE_URL_TASK_CAT = "http://taskcatdev.azurewebsites.net";
     public static final String PATH_POST_ORDER = "/api/Order";
