c03dd75e3574f655a4d5e8dc514c178fd7b1ac4e
Fahim Masud Choudhury <fahim@gobd.co>
2016-03-21 13:04:11 +0600

    BASE_URL has been updated to taskcatdev.azurewebsites.net

--- app/src/main/java/co/gobd/gofetch/config/Url.java
+++ app/src/main/java/co/gobd/gofetch/config/Url.java
@@ -5,9 +5,8 @@ package co.gobd.gofetch.config;
  */
 public class Url {
     // Supported order
-    public static final String BASE_URL = "http://192.168.0.136:8080";
-    public static final String PATH_SUPPORTED_ORDER = "/api/Order/SupportedOrder.json";
-    public static final String BASE_URL_TASK_CAT = "http://taskcatdev.azurewebsites.net";
+    public static final String BASE_URL = "http://taskcatdev.azurewebsites.net/";
+    public static final String PATH_SUPPORTED_ORDER = "/api/order/supportedorder";
     public static final String PATH_POST_ORDER = "/api/Order";
     private Url() {
     }
