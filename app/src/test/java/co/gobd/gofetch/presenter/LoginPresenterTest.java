5dfb46f9d81aa154a561903d96220a59859359d6
Fahim Masud Choudhury <fahim@gobd.co>
2016-05-15 20:03:22 +0600

    GFETCh-38 Unit test added
    Added unit tests for LoginPresenter#isValidCredentials() method, passing

---
+++ app/src/test/java/co/gobd/gofetch/presenter/LoginPresenterTest.java
@@ -0,0 +1,79 @@
+package co.gobd.gofetch.presenter;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import co.gobd.gofetch.service.account.AccountService;
+import co.gobd.gofetch.ui.view.LoginView;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by tonmoy on 15-May-16.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class LoginPresenterTest {
+
+    @Mock
+    AccountService service;
+
+    @Mock
+    LoginView view;
+
+    private LoginPresenter presenter;
+
+    @Before
+    public void setUp() throws Exception {
+        presenter = new LoginPresenter(service);
+        presenter.initialise(view);
+    }
+
+    @Test
+    public void shouldShowUsernameEmptyError(){
+
+        when(view.getUserName()).thenReturn("");
+        presenter.isValidCredentials();
+        verify(view).showUserNameEmptyError();
+
+    }
+
+    @Test
+    public void shouldShowPasswordEmptyError(){
+
+        when(view.getUserName()).thenReturn("fahim");
+        when(view.getPassword()).thenReturn("");
+        presenter.isValidCredentials();
+        verify(view).showPasswordEmptyError();
+
+    }
+
+    @Test
+    public void shouldShowPasswordLengthError(){
+
+        when(view.getUserName()).thenReturn("fahim");
+        when(view.getPassword()).thenReturn("123");
+        presenter.isValidCredentials();
+        verify(view).showPasswordLengthError();
+    }
+
+    @Test
+    public void shouldReturnTrueWhenValidCredentials(){
+
+        when(view.getUserName()).thenReturn("fahim");
+        when(view.getPassword()).thenReturn("123456");
+        presenter.isValidCredentials();
+        assertEquals(presenter.isValidCredentials(), true);
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
