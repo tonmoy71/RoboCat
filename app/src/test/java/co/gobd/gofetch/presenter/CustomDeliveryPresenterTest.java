3548bcdd2ae80e192c0e1501bbcce5cb44e6bec9
Fahim Masud Choudhury <fahim@gobd.co>
2016-06-01 20:07:07 +0600

    GFETCH-158 Unit test for getting item

--- app/src/test/java/co/gobd/gofetch/presenter/CustomDeliveryPresenterTest.java
+++ app/src/test/java/co/gobd/gofetch/presenter/CustomDeliveryPresenterTest.java
@@ -25,21 +25,33 @@ public class CustomDeliveryPresenterTest {
     @Before
     public void setUp() throws Exception {
 
+
         presenter = new CustomDeliveryPresenter();
 
-        String item1 = "Baklava from At-teen restaurant";
-        String item2 = "2kg Rice from Agora";
-        int quantity = 5;
-
-        packageLists = new ArrayList<>();
-        packageLists.add(new PackageList(item1, quantity));
-        packageLists.add(new PackageList(item2, quantity));
     }
 
     @Test
-    public void shouldPopulatePackageList() {
+    public void shouldAddItemToPackageList() {
+
+        packageLists = new ArrayList<>();
+        packageLists.add(new PackageList("Baklava from At-teen restaurant", 5));
+        packageLists.add(new PackageList("2kg Rice from Agora", 1));
         packageLists.add(new PackageList("Laptop charger from my house", 1));
+
         assertEquals(3, packageLists.size());
+    }
+
+    @Test
+    public void shouldGetPackageFromList()
+    {
+        packageLists = new ArrayList<>();
+        packageLists.add(new PackageList("Baklava from At-teen restaurant", 5));
+        packageLists.add(new PackageList("2kg Rice from Agora", 1));
+        packageLists.add(new PackageList("Laptop charger from my house", 1));
+
+        PackageList item = packageLists.get(0);
+        assertEquals(5, item.quantity);
+
     }
 
 
